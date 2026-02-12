d7870e92b3fabe8bbf2bbc728c8d5aa1325615f0
iPrisc <florent.lelion@gmail.com>
2026-02-12 15:40:21 +0100

    resolve conflict

--- src/handEvaluator.js
+++ src/handEvaluator.js
@@ -1,22 +1,5 @@
 // Évalue une main de 5 cartes et retourne sa catégorie
 export function evaluateHand(cartes) {
-<<<<<<< HEAD
-
-=======
-  // Validation : vérifier qu'il n'y a pas de cartes en double
-  const cartesVues = new Set()
-  
-  for (const carte of cartes) {
-    const carteCle = carte.card // Ex: "RC", "7D", etc.
-    
-    if (cartesVues.has(carteCle)) {
-      throw new Error('Cartes en double détectées')
-    }
-    
-    cartesVues.add(carteCle)
-  }
-  
->>>>>>> 7562e88675d3c0d61aef32c5201966bca1fb466b
   // Étape 1 : Compter combien de fois chaque rang apparaît
   const compteur = {}
 
